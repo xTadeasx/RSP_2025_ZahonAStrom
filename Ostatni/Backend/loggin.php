1a5dda649570efe69f66efc25e4b6f24e03b0e73
xTadeasx <jahoda.tadeas@gmail.com>
2025-10-27 14:15:23 +0100

    pčepojení loginu do datacontrol

--- Ostatni/Backend/loggin.php
+++ Ostatni/Backend/loggin.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/notAccess.php'; // !!! KOPÍROVAT DO KAŽDÉ CHRÁNĚNÉ STRÁNKY !!!
-require_once __DIR__ . '/../Database/db.php';
+require_once __DIR__ . '/../Database/dataControl.php'; // Připojení k DB a funkce pro práci s daty 
 
 // To do: Vytvořit způsob přihlašování uživatele
 ?>
